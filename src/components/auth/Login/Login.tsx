195005e66f19c5d8ff8b6de6b0bda9f0d4263f9f
Ori Roll <oriroll@gmail.com>
2025-02-02 05:53:56 +0200

    added image to login

--- src/components/auth/Login/Login.tsx
+++ src/components/auth/Login/Login.tsx
@@ -8,10 +8,11 @@ import {
   Divider,
   Group,
   Stack,
-  BackgroundImage,
+  Flex,
+  Center,
 } from '@mantine/core';
 
-import Image from '../../../assets/background.svg';
+import YourSvg from '../../../assets/pigibank_all.svg';
 
 const redirectToLoginWithGoogle = async () => {
   window.location.href = `http://${import.meta.env.VITE_BASE_API}/auth/google`;
@@ -27,46 +28,47 @@ export default function LoginPage() {
   };
 
   return (
-    <BackgroundImage src={Image} radius="100px">
-      <Title ta="center" order={2} pt={40} pb={20}>
-        BANANA BANK
-      </Title>
-      <Paper w="500px" radius="100px" p={60} pt={30}>
-        <form onSubmit={handleSubmit}>
-          <Stack mt="xl">
-            <TextInput
-              radius={20}
-              label="Email"
-              placeholder="hello@example.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
+    <Center h="100vh">
+      <Flex direction="row">
+        <Paper w="360px">
+          <Title ta="center" order={2} pt={40} pb={20}>
+            BANANA BANK
+          </Title>
+          <form onSubmit={handleSubmit}>
+            <Stack mt="xl">
+              <TextInput
+                label="Email"
+                placeholder="hello@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
 
-            <PasswordInput
-              radius={20}
-              label="Password"
-              placeholder="Your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
+              <PasswordInput
+                label="Password"
+                placeholder="Your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
 
-            <Button type="submit" fullWidth mt="xl">
-              Sign in
+              <Button variant="outline" type="submit" fullWidth mt="xl">
+                Sign in
+              </Button>
+            </Stack>
+          </form>
+
+          <Divider label="Or continue with" labelPosition="center" my="lg" />
+
+          <Group grow mb="md" mt="md">
+            <Button variant="default" onClick={redirectToLoginWithGoogle}>
+              Google
             </Button>
-          </Stack>
-        </form>
-
-        <Divider label="Or continue with" labelPosition="center" my="lg" />
-
-        <Group grow mb="md" mt="md">
-          <Button variant="default" onClick={redirectToLoginWithGoogle}>
-            Google
-          </Button>
-          <Button variant="default">Facebook</Button>
-        </Group>
-      </Paper>
-    </BackgroundImage>
+            <Button variant="default">Facebook</Button>
+          </Group>
+        </Paper>
+        <img style={{ height: '680px' }} src={YourSvg} />
+      </Flex>
+    </Center>
   );
 }
