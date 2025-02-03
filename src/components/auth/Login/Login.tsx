6fb77d5ccafcb269351f3e3d3c40c29273298ba4
Ori Roll <oriroll@gmail.com>
2025-02-03 06:07:19 +0200

    login style update

--- src/components/auth/Login/Login.tsx
+++ src/components/auth/Login/Login.tsx
@@ -10,9 +10,11 @@ import {
   Stack,
   Flex,
   Center,
+  FlexProps,
 } from '@mantine/core';
 
 import YourSvg from '../../../assets/pigibank_all.svg';
+import { useIsMobile } from '../../../hooks/configHooks';
 
 const redirectToLoginWithGoogle = async () => {
   window.location.href = `http://${import.meta.env.VITE_BASE_API}/auth/google`;
@@ -22,15 +24,28 @@ export default function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const isMobile = useIsMobile();
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // Add login logic here
   };
 
+  const layoutStyle = {
+    direction: isMobile ? 'column' : 'row',
+    align: 'center',
+    w: isMobile ? '100%' : '1000px',
+    p: isMobile ? '30px' : '40px',
+  } satisfies FlexProps;
+
+  const paperStyle = {
+    width: !isMobile ? '100%' : '200px',
+  } as const;
+
   return (
-    <Center h="100vh">
-      <Flex direction="row">
-        <Paper w="360px">
+    <Center w="100%">
+      <Flex {...layoutStyle}>
+        <Paper w="100%">
           <Title ta="center" order={2} pt={40} pb={20}>
             BANANA BANK
           </Title>
@@ -67,7 +82,7 @@ export default function LoginPage() {
             <Button variant="default">Facebook</Button>
           </Group>
         </Paper>
-        <img style={{ height: '680px' }} src={YourSvg} />
+        <img style={{ height: '620px' }} src={YourSvg} />
       </Flex>
     </Center>
   );
